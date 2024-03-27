60712c653bc493d5ffdc044d925e82013cedd5f9
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-03-27 12:25:58 +0000

    Finished Drop Drop Menu (Non-mobile, No-animation)

--- src/index.js
+++ src/index.js
@@ -2,76 +2,26 @@ import { forEach } from "lodash";
 import "./style.css";
 
 const topMenus = document.querySelectorAll('.top-menus')
-const portfolio = document.querySelector('#portfolio')
-const portfolioOptions = portfolio.querySelectorAll('li')
-const contact = document.querySelector('#contact')
-const contactOptions = contact.querySelectorAll('li')
 
-window.addEventListener('load', (e) => {
-    // hidePortfolioByDefault()
-    // hidecontactByDefault()
-    // showPortfolioDropDown()
-    // showContactDropDown()
-    // topMenus[i].children[1].style.display = 'none'
-    optionMenu()
+window.addEventListener('load', () => {
+    topMenus.forEach(menu => {
+        menu.children[1].style.display = 'none'
+    })
+    addListenersToTopMenus()
 })
 
-function optionMenu() {
+function addListenersToTopMenus() {
     for (let i = 0; i < topMenus.length; i++) {
-        // console.log(topMenus[i])
         topMenus[i].addEventListener('mouseover', () => {
-            topMenus[i].children[1].style.display = 'block'
-
+            showOrHideDropDown(topMenus[i])
         })
         topMenus[i].addEventListener('mouseout', () => {
-            topMenus[i].children[1].style.display = 'none'
-            // topMenus[i].children[1].classList.add
-            // showOrHide(topMenus[i].children[1])
-
+            showOrHideDropDown(topMenus[i])
         })
-
     }
 }
 
-function showOrHide(menuOptions) {
-
-    // if (topMenus[i].children[1].style.display = 'block') {
-
-    // }
+function showOrHideDropDown(menus) {
+    const dropDown = menus.children[1]
+    dropDown.style.display = dropDown.style.display == 'none' ? 'block' : 'none'
 }
-
-// function hidePortfolioByDefault() {
-//     hide(portfolioOptions)
-// }
-
-// function hidecontactByDefault() {
-//     hide(contactOptions)
-// }
-
-// function hide(dropDownMenu) {
-//     dropDownMenu.forEach(option => {
-//         option.addEventListener('mouseout', (e) => {
-//             // console.log(option)
-//             option.style.display = 'none'
-//         })
-//     })
-// }
-
-// function showPortfolioDropDown() {
-//     console.log('show portfolio')
-//     portfolio.addEventListener('mouseover', (e) => {
-//         show(portfolioOptions)
-//         portfolio.style.backgroundColor = 'grey'
-//     })
-// }
-
-// function showContactDropDown() {
-//     console.log('show contact')
-    
-// }
-
-// function show(dropDownMenu) {
-//     dropDownMenu.forEach(option => {
-//         option.style.display = 'block'
-//     })
-// }
