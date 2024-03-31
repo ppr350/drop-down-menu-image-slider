17f8ea969034205a33c142757c2cb3fa6bc547fc
ppr350 <kobito610@gmail.com>
2024-03-31 16:25:59 +0100

    Copied code from the web to test out

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { forEach } from "lodash";
 import "./style.css";
 
+// Drop Drop Menu
+
 const topMenus = document.querySelectorAll('.top-menus')
 
 window.addEventListener('load', () => {
@@ -34,3 +36,28 @@ function showOrHideDropDown(menus) {
     const dropDown = menus.children[1]
     dropDown.style.display = dropDown.style.display == 'none' ? 'block' : 'none'
 }
+
+// Image Slider
+const images = document.querySelectorAll('#image-slider img')
+const dots = document.querySelectorAll('.dot')
+let currentImage = 0
+const interval = 3000
+
+function changeImage(toCurrentImage) {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.opacity = 0
+        dots[i].classList.remove('active')
+    }
+    currentImage = (currentImage + 1) % images.length
+
+    images[currentImage].style.opacity = 1
+    dots[currentImage].classList.add('active')
+
+    if (toCurrentImage != undefined) {
+        clearInterval(timer)
+        timer = setInterval(changeImage, interval)
+        currentImage = toCurrentImage
+    }
+}
+
+const timer = setInterval(changeImage, interval)
