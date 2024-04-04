9107dd5451b64c7ccf1d5a642be8ed896df0be4c
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-04-04 11:52:40 +0100

    Change config path from dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, "/build"),
     assetModuleFilename: "[name][ext]",
     clean: true,
   },
